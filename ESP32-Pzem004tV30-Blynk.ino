07e531a34cde93a1e9f63bc61b22745ae0840aaf
DocTeeraThxp <106339662+Teera235@users.noreply.github.com>
2024-04-10 10:46:07 +0700

    Update ESP32-Pzem004tV30-Blynk.ino

--- ESP32-Pzem004tV30-Blynk.ino
+++ ESP32-Pzem004tV30-Blynk.ino
@@ -1,5 +1,5 @@
-#define BLYNK_TEMPLATE_ID "xxxxxxxxxxxxxxxx"
-#define BLYNK_TEMPLATE_NAME "xxxxxxxxxxxx"
+#define BLYNK_TEMPLATE_ID "xxxxxxxxxxx"
+#define BLYNK_TEMPLATE_NAME "xxxxxxxxxxx"
 #define BLYNK_AUTH_TOKEN "xxxxxxxxxxx"
 
 #include <BlynkSimpleEsp32.h>
@@ -8,11 +8,14 @@
 #include <WiFiClient.h>
 #include <TridentTD_LineNotify.h>
 #include <HTTPClient.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h> 
+
 
 char ssid[] = "xxxxxxxxxxx";
-char pass[] = "xxxxxxxxxxxxx";
+char pass[] = "xxxxxxxxxxx";
 
-char LINE_TOKEN[] = "xxxxxxxxxxxxxxxxxxx";
+char LINE_TOKEN[] = "xxxxxxxxxxx";
 
 #if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
 #define PZEM_RX_PIN 16
@@ -20,7 +23,7 @@ char LINE_TOKEN[] = "xxxxxxxxxxxxxxxxxxx";
 #endif
 
 #if !defined(PZEM_SERIAL)
-#define PZEM_SERIAL Serial2
+#define PZEM_SERIAL Serial1
 #endif
 
 PZEM004Tv30 pzem(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN);
@@ -33,6 +36,8 @@ bool ledState = LOW;
 
 unsigned long lastNotificationTime = 0;
 const unsigned long notificationInterval = 60000*60; 
+
+LiquidCrystal_I2C lcd(0x27, 16, 4);
 
 void relayOn(int relayNumber) {
   switch (relayNumber) {
@@ -90,6 +95,12 @@ void setup() {
   LINE.setToken(LINE_TOKEN);
   LINE.notify("ระบบวัดไฟฟ้าเริ่่ม!!!");
   delay(2000);
+  
+  lcd.init();                   
+  lcd.backlight();               
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing...");
+  delay(2000);
 }
 
 BLYNK_WRITE(V0) {
@@ -119,19 +130,19 @@ void loop() {
   float energy = pzem.energy();
   float frequency = pzem.frequency();
   float pf = pzem.pf();
-  float electricityCostPerUnit = 4.54; 
+  float electricityCostPerUnit = 4.54;
   float energyCost = (energy / 1000.0) * electricityCostPerUnit;
-  
+
   if (isnan(voltage) || isnan(current) || isnan(power) || isnan(energy) || isnan(frequency) || isnan(pf)) {
     Serial.println("Error reading sensor values");
   } else {
     if (millis() - lastNotificationTime >= notificationInterval) {
-      String notificationMessage = 
-                                   "Voltage: " + String(voltage) + " V\n"
-                                 + "Current: " + String(current) + " A\n"
-                                 + "Power: " + String(power) + " W\n"
-                                 + "Energy: " + String(energyCost / 1000.0) + " kWh (" + String(energyCost) + " บาท)\n";
-                                 
+      String notificationMessage =
+        "Voltage: " + String(voltage) + " V\n"
+        + "Current: " + String(current) + " A\n"
+        + "Power: " + String(power) + " W\n"
+        + "Energy: " + String(energyCost / 1000.0) + " kWh (" + String(energyCost) + " บาท)\n";
+
       sendToLineNotify(notificationMessage);
       lastNotificationTime = millis();
     }
@@ -147,10 +158,32 @@ void loop() {
     Blynk.virtualWrite(V1, voltage);
     Blynk.virtualWrite(V2, current);
     Blynk.virtualWrite(V3, power);
-    Blynk.virtualWrite(V4, energy / 1000.0); 
+    Blynk.virtualWrite(V4, energy / 1000.0);
     Blynk.virtualWrite(V5, frequency);
     Blynk.virtualWrite(V6, pf);
     Blynk.virtualWrite(V8, energyCost);
+
+    // Update LCD
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Voltage: ");
+    lcd.print(voltage);
+    lcd.print("V");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Current: ");
+    lcd.print(current);
+    lcd.print("A");
+
+    lcd.setCursor(0, 2);
+    lcd.print("Power: ");
+    lcd.print(power);
+    lcd.print("W");
+
+    lcd.setCursor(0, 3);
+    lcd.print("Energy: ");
+    lcd.print(energy / 1000.0);
+    lcd.print("kWh");
   }
 
   Serial.println();
